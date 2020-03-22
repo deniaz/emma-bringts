2e5ade25dbb3b79140318756975f4854974a1520
Robert Vogt <robert@smartive.ch>
2020-03-22 21:28:51 +0100

    Play with ENV vars

--- src/pages/api/geocode.ts
+++ src/pages/api/geocode.ts
@@ -2,15 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
 export default async ({ query }: NextApiRequest, res: NextApiResponse) => {
+  const key = process.env.OPENCAGEDATA_API_KEY;
   const zip = query['zip'].toString();
   if (!zip) {
     res.statusCode = 400;
     res.end();
   }
 
-  const response = await fetch(
-    `https://api.opencagedata.com/geocode/v1/json?key=4cbf0f470fba41f88e45fc26349e8473&q=${zip}+schweiz`
-  );
+  const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=${key}&q=${zip}+schweiz`);
 
   if (!response.ok) {
     res.statusCode = 404;
--- src/pages/suche.tsx
+++ src/pages/suche.tsx
@@ -26,7 +26,9 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const lat = parseFloat(query['lat'].toString());
   const lng = parseFloat(query['lng'].toString());
 
-  const client = new MongoClient(process.env.MONGODB_HOST, { useUnifiedTopology: true });
+  console.info(process.env.MONGO_DB_HOST);
+
+  const client = new MongoClient(process.env.MONGO_DB_HOST, { useUnifiedTopology: true });
 
   await client.connect();
 
