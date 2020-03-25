32645699cfbd1fd18a96e0abe91189b7c32bc44b
Robert Vogt <robert@smartive.ch>
2020-03-25 22:15:22 +0100

    Add Padding for mobile

--- src/layout/stacked.tsx
+++ src/layout/stacked.tsx
@@ -6,7 +6,7 @@ type Props = {
   children: ReactNode;
 };
 
-const styles = { container: 'bg-gray-100 box-border', main: 'py-8 max-w-screen-xl mx-auto box-border' };
+const styles = { container: 'bg-gray-100 box-border px-8 lg:px-0', main: 'py-8 max-w-screen-xl mx-auto box-border' };
 
 export const Stacked: FC<Props> = ({ children }) => (
   <div className={styles.container}>
