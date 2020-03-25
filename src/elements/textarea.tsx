62ad2e3bd8b3afdd28b842e6602f926666c28594
Robert Vogt <robert@smartive.ch>
2020-03-25 22:10:41 +0100

    Add font declaration to Input and Textarea

--- src/elements/textarea.tsx
+++ src/elements/textarea.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, FC } from 'react';
 const styles = {
   container: 'inline-block w-full mb-6',
   label: 'block font-sans text-gray-800 font-normal text-sm mb-2',
-  textarea: 'w-full bg-white rounded-md border border-gray-400 text-base py-2 px-4',
+  textarea: 'w-full bg-white rounded-md border border-gray-400 text-base py-2 px-4 font-sans',
 };
 
 type Props = {
