9df6bde3f7a0023bd59cffb78bf96c42c55b1e66
Peter Manser <peter@smartive.ch>
2020-03-25 21:55:07 +0100

    Lighter tag

--- src/elements/tag.tsx
+++ src/elements/tag.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 
 const styles = {
   tag:
-    'font-sans rounded-full bg-indigo-500 text-white text-xs font-normal py-1 px-4 mr-1 mb-1 flex-row fill-current inline-flex',
+    'font-sans rounded-full bg-indigo-400 text-white text-xs font-normal py-1 px-4 mr-1 mb-1 flex-row fill-current inline-flex',
 };
 
 type Props = { children: ReactNode };
