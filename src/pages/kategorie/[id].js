68bd7c3b1186d9e42b5a2d28639109629e762183
Peter Manser <peter@smartive.ch>
2020-03-30 20:51:02 +0200

    Server side

--- src/pages/kategorie/[id].js
+++ src/pages/kategorie/[id].js
@@ -2,11 +2,19 @@ import { useRouter } from 'next/router';
 import { SearchWithResults } from '../../compositions/search-with-results';
 import { Stacked } from '../../layout/stacked';
 
-export default () => {
-  const { query } = useRouter();
+export async function getServerSideProps(context) {
+  const { params, query } = context;
+  return {
+    props: { params, query },
+  };
+}
+
+export default (props) => {
+  const { params, query } = props;
+
   return (
     <Stacked title="Anbieter und Angebote - Emma bringts!">
-      <SearchWithResults selectedCategory={query.id} query={query} />
+      <SearchWithResults selectedCategory={params.id} query={query} />
     </Stacked>
   );
 };
