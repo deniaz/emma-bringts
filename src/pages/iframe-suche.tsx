4cf07d30b01a1df80c51897c88a0991d9f4c28de
Robert Vogt <robert@smartive.ch>
2020-04-04 20:52:15 +0200

    feat: Apply filters to Categories as well

--- src/pages/iframe-suche.tsx
+++ src/pages/iframe-suche.tsx
@@ -1,16 +1,11 @@
 import { SearchWithResults } from '../compositions/search-with-results';
-import { getClient } from '../services/mongo';
 
 export async function getServerSideProps(context) {
-  const client = await getClient();
-  const categories = await client.db('shops').collection('shops').distinct('categories');
-  client.close();
-
   return {
-    props: { query: context.query, categories },
+    props: { query: context.query },
   };
 }
 
-export default ({ query, categories }) => {
-  return <SearchWithResults categories={categories} query={query} />;
+export default ({ query }) => {
+  return <SearchWithResults query={query} />;
 };
