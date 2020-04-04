57bfeb064eb672da100fa4d15aed138837fd1d6c
Robert Vogt <robert@smartive.ch>
2020-04-05 00:21:14 +0200

    Re-Introduce IFrame Page

--- src/pages/iframe-suche.tsx
+++ src/pages/iframe-suche.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps } from 'next';
 import { useEffect, useMemo } from 'react';
 import useSWR, { responseInterface, useSWRPages } from 'swr';
 import { Vendor } from '../components/vendor';
-import { Hero } from '../compositions/hero';
 import { SearchResults } from '../compositions/search-results';
 import { Tag } from '../elements/tag';
 import { ZipSearch } from '../elements/zip-search';
@@ -11,32 +10,33 @@ import { useGetCurrentPosition } from '../hooks/use-get-current-position';
 import { getArrayFromQuery, useQueryParamState } from '../hooks/use-query-param-state';
 import { Stacked } from '../layout/stacked';
 import { fetcher } from '../services/api';
+import cn from '../utils/classname';
 
 const styles = {
-  zipContainer: 'my-20',
+  hero: 'py-16',
 };
 
 const query = `query Vendors($service: [Service!], $zip: Int, $tenants: [Tenant!], $categories: [String], $skip: Int!, $limit: Int!) {
-  vendors(filter: {service: $service, tenants: $tenants, zip: $zip, categories: $categories}, skip: $skip, limit: $limit) {
-    id
-    name
-    categories
-    body
-    service
-    hours
-    address
-    contact
-  }
-  categories(filter:{service: $service, tenants: $tenants, zip: $zip})
-  total
-}`;
+    vendors(filter: {service: $service, tenants: $tenants, zip: $zip, categories: $categories}, skip: $skip, limit: $limit) {
+      id
+      name
+      categories
+      body
+      service
+      hours
+      address
+      contact
+    }
+    categories(filter:{service: $service, tenants: $tenants, zip: $zip})
+    total
+  }`;
 
-export default ({ zip = '', categories = [] }) => {
-  const initial = { zip, categories };
+export default ({ zip = '', categories = [], tenants = [] }) => {
+  const initial = { zip, categories, tenants };
   const [state, dispatch] = useQueryParamState(initial);
   const [, postcode] = useGetCurrentPosition();
 
-  const { pages, pageSWRs, isLoadingMore, isReachingEnd, loadMore } = useSWRPages(
+  const { pages, isLoadingMore, isReachingEnd, loadMore } = useSWRPages(
     'vendors',
     ({ offset, withSWR }) => {
       if (!zip) {
@@ -44,14 +44,14 @@ export default ({ zip = '', categories = [] }) => {
       }
 
       const variables = useMemo(() => {
-        const params = { categories: state.categories, limit: 10, skip: offset || 0 };
+        const params = { categories: state.categories, tenants: state.tenants, limit: 10, skip: offset || 0 };
 
         if (state.zip) {
           params['zip'] = parseInt(state.zip, 10);
         }
 
         return params;
-      }, [state.zip, state.categories]);
+      }, [state.zip, state.categories, state.tenants]);
 
       const { data } = withSWR(useSWR([query, variables], fetcher)) as responseInterface<
         { vendors: VendorType[]; total: number },
@@ -100,27 +100,26 @@ export default ({ zip = '', categories = [] }) => {
     });
   }
 
-  const [page] = pageSWRs;
-  const total = page?.data?.total;
-
   return (
     <Stacked title="Emma bringts! - Ein Verzeichnis von Unternehmen mit Abhol- oder Lieferservice.">
-      <Hero
-        search={
-          <div className={styles.zipContainer}>
-            <ZipSearch initial={state.zip} onChange={submit} />
+      <div className={cn(['hero-section', styles.hero])}>
+        <div className="container emma-container">
+          <ZipSearch initial={state.zip} onChange={submit} />
+        </div>
+
+        <div className="container category-outer">
+          <div className="category-container">
+            {/* eslint-disable-next-line @typescript-eslint/no-use-before-define */}
+            {filters.map(({ label, value }) => (
+              <Tag active={state.categories.includes(value)} key={value} label={label} onClick={() => toggleFilter(value)} />
+            ))}
+            <a href="#" className="category-item">
+              Alle anzeigen
+            </a>
           </div>
-        }
-        title={`${total ? `${total} ` : ''}Angebote${zip && ` in der Region ${zip}`}`}
-      >
-        {/* eslint-disable-next-line @typescript-eslint/no-use-before-define */}
-        {filters.map(({ label, value }) => (
-          <Tag active={state.categories.includes(value)} key={value} label={label} onClick={() => toggleFilter(value)} />
-        ))}
-        <a href="#" className="category-item">
-          Alle anzeigen
-        </a>
-      </Hero>
+        </div>
+      </div>
+
       <SearchResults>
         {pages}
         {!isReachingEnd && !isLoadingMore && (
@@ -136,12 +135,14 @@ export default ({ zip = '', categories = [] }) => {
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { zip } = query;
   const categories = getArrayFromQuery(query, 'categories');
+  const tenants = getArrayFromQuery(query, 'tenants');
 
   if (zip) {
     return {
       props: {
         zip,
         categories,
+        tenants,
       },
     };
   }
@@ -149,6 +150,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return {
     props: {
       categories,
+      tenants,
     },
   };
 };
